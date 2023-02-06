8b0547db92450090ad3cddd89ec544bc70ff5c2f
Bokobo Shahni <shahni@bokobo.space>
2023-02-05 19:38:04 -0500

    feat: add engine skeleton

---
+++ spec/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  mount Evematic::Engine, at: "/evematic"
+end
---
+++ spec/support/helpers/database_cleaner.rb
@@ -0,0 +1,11 @@
+require 'database_cleaner/active_record'
+
+module DatabaseCleaner
+  module ActiveRecord
+    class Base < DatabaseCleaner::Strategy
+      def self.migration_table_name
+        ::ActiveRecord::Base.connection.schema_migration.table_name
+      end
+    end
+  end
+end
